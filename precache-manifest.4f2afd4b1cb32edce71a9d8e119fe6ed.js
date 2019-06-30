470f1df30181b3e668131d1422a32983a7b9469b
Ramiro Gustavo Lopez <ramlopez@everis.com>
2019-06-30 00:00:53 -0300

    Updates

---
+++ precache-manifest.4f2afd4b1cb32edce71a9d8e119fe6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b712d60addb4fd76912c304b28ea4a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b06ddee804aafd5cab64",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c2f086c239e2725afb2e",
+    "url": "/static/js/2.68dee539.chunk.js"
+  },
+  {
+    "revision": "b06ddee804aafd5cab64",
+    "url": "/static/js/main.e4a6fbb1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
